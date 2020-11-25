5a40c22debeac20914168bc3d19793cf4cce9fcd
Sean C. Sullivan <github@seansullivan.com>
2020-11-24 21:34:21 -0800

    Scala 2.13.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,7 +20,7 @@ object Dependencies {
   val scala210 = "2.10.7"
   val scala211 = "2.11.12"
   val scala212 = "2.12.10"
-  val scala213 = "2.13.1"
+  val scala213 = "2.13.4"
 
   def scalaReflect(version: String): ModuleID =
     "org.scala-lang" % "scala-reflect" % version
