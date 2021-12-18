1ac63effa1d993010941df9d6af9745d7e8ddc56
PJ Fanning <pjfanning@users.noreply.github.com>
2021-12-18 12:06:19 +0000

    upgrades

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -26,7 +26,7 @@ object Dependencies {
   def scalaReflect(version: String) =
     if (version.startsWith("3")) None else Some("org.scala-lang" % "scala-reflect" % version)
 
-  private val log4jV = "2.16.0"
+  private val log4jV = "2.17.0"
   val osgiCoreApi      = "org.osgi" % "org.osgi.core" % "6.0.0" % Provided
   val log4jApi         = "org.apache.logging.log4j" % "log4j-api" % log4jV
   val log4jApiTests    = "org.apache.logging.log4j" % "log4j-api" % log4jV % Test classifier "tests"
@@ -38,5 +38,5 @@ object Dependencies {
   val scalatestMatcher = "org.scalatest" %% "scalatest-matchers-core" % "3.2.10" % Test
   val scalatestJunit   = "org.scalatestplus" %% "junit-4-13" % "3.2.10.0" % Test
   val scalatestMockito = "org.scalatestplus" %% "mockito-3-12" % "3.2.10.0" % Test
-  val mockito          = "org.mockito" % "mockito-core" % "3.12.0" % Test
+  val mockito          = "org.mockito" % "mockito-core" % "4.2.0" % Test
 }
